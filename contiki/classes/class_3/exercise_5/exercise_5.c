6a46563ed408374c0fd64a17636c1c9b305ab323
Rafael Ito <ito.rafael@gmail.com>
2022-09-28 17:43:39 -0300

    [contiki] class_3: exercise_5 (get-address)

---
+++ contiki/classes/class_3/exercise_5/exercise_5.c
@@ -0,0 +1,60 @@
+/*
+ * ===================
+ *  class 3: 
+ *  2022-09-22
+ *  get-address.c
+ * ===================
+ * Write a program that:
+ *   - sets a global IPv6 address and retrieves all the IPv6 addresses assigned to the interface of the node.
+ */
+
+//============================
+// includes
+//============================
+// general
+#include "contiki.h"
+#include <stdio.h>
+//----------------------------
+// IPv6
+#include "net/ip/uip.h"
+#include "net/ipv6/uip-ds6.h"
+#include "net/ip/uip-debug.h"
+//----------------------------
+
+//============================
+// IPv6 address
+//============================
+static uip_ipaddr_t ipaddr;
+
+//============================
+// process setup
+//============================
+// declare the process
+PROCESS(get_ip_addr, "Exercise 5: get IP addresses of interface");
+// start process after Contiki boot
+AUTOSTART_PROCESSES(&get_ip_addr);
+// define the process
+PROCESS_THREAD(get_ip_addr, ev, data) {
+
+    PROCESS_BEGIN();
+    //--------------------
+    // configuration
+    //--------------------
+    uip_ip6addr(&ipaddr, 0xabcd, 0, 0, 0, 0, 0, 0, 0);
+    uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+    uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+
+    int i;
+    printf("IPv6 addresses:\n");
+    //cycles for the max number of addresses that the interface can have
+    for(i = 0; i < UIP_DS6_ADDR_NB; i++){
+        if(uip_ds6_if.addr_list[i].isused){
+            //print the address
+            printf("  ");
+            uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
+            printf("\n");
+        }
+    }
+
+    PROCESS_END();
+}
---
+++ contiki/classes/class_3/exercise_5/project-conf.h
@@ -0,0 +1,3 @@
+// neighbor discovery
+#undef UIP_CONF_ND6_SEND_NA
+#define UIP_CONF_ND6_SEND_NA 1
