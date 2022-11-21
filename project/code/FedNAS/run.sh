42432bb74a06150ded87f4775580c285ea413e6d
Rafael Ito <ito.rafael@gmail.com>
2022-11-21 12:41:18 -0300

    [project] run.sh: add shell script to launch both server/client FL Python codes

---
+++ project/code/FedNAS/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+#cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
+# Download the CIFAR-10 dataset
+#python -c "from torchvision.datasets import CIFAR10; CIFAR10('./data', download=True)"
+
+#-------------------
+# start server
+#-------------------
+echo "Starting server"
+python server.py &
+sleep 3
+
+#-------------------
+# start clients
+#-------------------
+for i in $(seq $clients); do
+    echo "Starting client $i"
+    python client.py &
+done
+
+# Enable CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
+# Wait for all background processes to complete
+wait
